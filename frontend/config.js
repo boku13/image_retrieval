a4d795bb389ca8fe1a6b4007977f2e5677faf172
boku13 <b22ee036@iitj.ac.in>
2024-04-20 00:29:22 +0000

    domain

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 const config = {
-    apiUrl: (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') ? 'http://localhost:5000' : 'https://yourapp.com'
+    apiUrl: (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') ? 'http://localhost:5000' : 'https://boku13.github.io'
 };
 console.log(config.apiUrl)
