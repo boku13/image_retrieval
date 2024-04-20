a298b8e7bef8634b88e30575e69dc2d8d23d0ee8
boku13 <b22ee036@iitj.ac.in>
2024-04-20 18:38:56 +0000

    domain

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 const config = {
-    apiUrl: (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') ? 'http://localhost:5000' : 'http://boku.pythonanywhere.com/'
+    apiUrl: (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') ? 'http://localhost:5000' : 'http://boku.pythonanywhere.com'
 };
 console.log(config.apiUrl)
