167a4771e1ff7b1dc73e740045b75c694ae618d6
boku13 <b22ee036@iitj.ac.in>
2024-04-20 16:59:00 +0000

    config

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 const config = {
-    apiUrl: (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') ? 'http://localhost:5000' : 'https://boku13.github.io/image_retrieval/frontend'
+    apiUrl: (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') ? 'http://localhost:5000' : 'https://image-retrieval-smoky.vercel.app/'
 };
 console.log(config.apiUrl)
