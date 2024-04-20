b9c733829dd95467fa44e65e197d1ed9394ca380
boku13 <b22ee036@iitj.ac.in>
2024-04-20 00:34:25 +0000

    domain

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 const config = {
-    apiUrl: (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') ? 'http://localhost:5000' : 'https://boku13.github.io'
+    apiUrl: (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') ? 'http://localhost:5000' : 'https://boku13.github.io/image_retrieval/frontend'
 };
 console.log(config.apiUrl)
